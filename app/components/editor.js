651e5ffd19395c508664c5bfc0779cb9aa9a836e
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2021-08-31 16:36:03 +0200

    added editor to exercise part 1

--- app/components/editor.js
+++ app/components/editor.js
@@ -1,10 +1,10 @@
-import Controller from '@ember/controller';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-export default class PlaygroundController extends Controller {
+export default class EditorComponent extends Component {
   @tracked
-  endpoint = 'https://dbpedia.org/sparql';
+  endpoint = 'http://sparql-demo.s.redpencil.io:8890/sparql';
 
   @action
   setEndpoint(event) {
