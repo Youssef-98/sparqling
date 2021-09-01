43d8b573bc3c375428d5b6b1672a1bcde95e8ed1
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2021-09-01 14:31:18 +0200

    currently changed to popup instead of modal

--- app/components/exercise-one.js
+++ app/components/exercise-one.js
@@ -1,3 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class ExerciseOneComponent extends Component {}
+export default class ExerciseOneComponent extends Component {
+  @tracked isOpen = false;
+
+  @action
+  showSnippet() {
+    this.isOpen = !this.isOpen;
+    return this.isOpen;
+  }
+}
